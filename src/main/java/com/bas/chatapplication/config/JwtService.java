cdf3ac704c5f986784bd77cc269999510426b3e5
Bas Onrust <74906654+Saprone@users.noreply.github.com>
2023-10-23 16:01:01 +0200

    Update JwtService.java

--- src/main/java/com/bas/chatapplication/config/JwtService.java
+++ src/main/java/com/bas/chatapplication/config/JwtService.java
@@ -1,11 +1,74 @@
 package com.bas.chatapplication.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.security.Key;
 
 @Service
 public class JwtService {
 
+    private  static final String SECRET_KEY = "c606de64c81a4c4e8549bac747ef5d15cef225efcbc79d15d3331f018614ea86";
+
     public String extractUsername(String jwtToken) {
-        return null;
+        return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(jwtToken);
+
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims (String jwtToken) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody();
+    }
+
+    public  String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15 ))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String jwtToken, UserDetails userDetails) {
+        final String username = extractUsername(jwtToken);
+
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(jwtToken);
+    }
+
+    private boolean isTokenExpired(String jwtToken) {
+        return extractExpirationDate(jwtToken).before(new Date());
+    }
+
+    private Date extractExpirationDate(String jwtToken) {
+       return extractClaim(jwtToken, Claims::getExpiration);
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
