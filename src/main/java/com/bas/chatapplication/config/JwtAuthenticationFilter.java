7f0e75ac164d9d34221e5ae275c02b26d0428300
Bas Onrust <74906654+Saprone@users.noreply.github.com>
2023-10-23 16:20:00 +0200

    Update JwtAuthenticationFilter.java

--- src/main/java/com/bas/chatapplication/config/JwtAuthenticationFilter.java
+++ src/main/java/com/bas/chatapplication/config/JwtAuthenticationFilter.java
@@ -6,9 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
@@ -35,7 +37,21 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         userName = jwtService.extractUsername(jwtToken);
 
         if(userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.userDetailsService.loadByUsername(userName);
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userName);
+
+            if(jwtService.isTokenValid(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
         }
     }
 }
