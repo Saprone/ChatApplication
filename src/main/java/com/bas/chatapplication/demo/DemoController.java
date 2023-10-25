d0a652cc9b189fbc9c884d165243315b09436614
Bas Onrust <74906654+Saprone@users.noreply.github.com>
2023-10-25 17:27:35 +0200

    Update DemoController.java

--- src/main/java/com/bas/chatapplication/demo/DemoController.java
+++ src/main/java/com/bas/chatapplication/demo/DemoController.java
@@ -1,5 +1,6 @@
 package com.bas.chatapplication.demo;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,8 @@ public class DemoController {
 
     @GetMapping
     public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Hello from secured endpoint.");
+        //return ResponseEntity.ok("Hello from secured endpoint.");
+        return new ResponseEntity<>("Bad request test.", HttpStatus.BAD_REQUEST);
     }
 
 }
