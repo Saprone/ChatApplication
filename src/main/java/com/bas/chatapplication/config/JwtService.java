a31009e97d4d9a05885f562d00eb5a432d44e049
Bas Onrust <74906654+Saprone@users.noreply.github.com>
2023-10-26 14:30:30 +0200

    Refactor generateToken()

--- src/main/java/com/bas/chatapplication/config/JwtService.java
+++ src/main/java/com/bas/chatapplication/config/JwtService.java
@@ -48,12 +48,16 @@ public class JwtService {
     }
 
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return buildToken(extraClaims, userDetails, accessTokenExpiration);
+    }
+
+    private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, long accessTokenExpiration) {
         return Jwts
             .builder()
             .setClaims(extraClaims)
             .setSubject(userDetails.getUsername())
             .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpiration ))
+            .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpiration))
             .signWith(getSignInKey(), SignatureAlgorithm.HS256)
             .compact();
     }
