6c6623fca678cc2260ccf27c25b1ccc7b57a34f1
Bas Onrust <74906654+Saprone@users.noreply.github.com>
2023-10-30 15:45:51 +0100

    Added OneToMany relationship tokens from user

--- src/main/java/com/bas/chatapplication/token/Token.java
+++ src/main/java/com/bas/chatapplication/token/Token.java
@@ -1,5 +1,6 @@
 package com.bas.chatapplication.token;
 
+import com.bas.chatapplication.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,9 +17,15 @@ public class Token {
     @Id
     @GeneratedValue
     private Integer id;
+
     private String token;
     @Enumerated(EnumType.STRING)
     private TokenType tokenType;
+
     private boolean expired;
     private boolean revoked;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
