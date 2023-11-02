ed0c91533818c5aad13a7d347200b78daca833d5
Bas Onrust <74906654+Saprone@users.noreply.github.com>
2023-11-02 14:43:11 +0100

    Secured endpoints admin, manager, user

--- src/main/java/com/bas/chatapplication/config/SecurityConfiguration.java
+++ src/main/java/com/bas/chatapplication/config/SecurityConfiguration.java
@@ -11,6 +11,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+import static com.bas.chatapplication.user.Permission.*;
+import static com.bas.chatapplication.user.Role.*;
+import static org.springframework.http.HttpMethod.*;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -29,6 +33,25 @@ public class SecurityConfiguration {
             .authorizeHttpRequests(req ->
                 req.requestMatchers("/api/v1/auth/**")
                     .permitAll()
+
+                    .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
+                    .requestMatchers(GET, "/api/v1/admin/**").hasAuthority(ADMIN_READ.name())
+                    .requestMatchers(POST, "/api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
+                    .requestMatchers(PUT, "/api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
+                    .requestMatchers(DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())
+
+                    .requestMatchers("/api/v1/manager/**").hasAnyRole(ADMIN.name(), MANAGER.name())
+                    .requestMatchers(GET, "/api/v1/manager/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
+                    .requestMatchers(POST, "/api/v1/manager/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
+                    .requestMatchers(PUT, "/api/v1/manager/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
+                    .requestMatchers(DELETE, "/api/v1/manager/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
+
+                    .requestMatchers("/api/v1/user/**").hasAnyRole(ADMIN.name(), MANAGER.name(), USER.name())
+                    .requestMatchers(GET, "/api/v1/user/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name(), USER_READ.name())
+                    .requestMatchers(POST, "/api/v1/user/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name(), USER_CREATE.name())
+                    .requestMatchers(PUT, "/api/v1/user/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name(), USER_UPDATE.name())
+                    .requestMatchers(DELETE, "/api/v1/user/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name(), USER_DELETE.name())
+
                     .anyRequest()
                     .authenticated()
             )
