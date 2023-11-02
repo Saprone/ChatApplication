a0e5af9041077fa5da29336f5aff4584d22c59c6
Bas Onrust <74906654+Saprone@users.noreply.github.com>
2023-11-02 16:42:46 +0100

    Added register.builder admin, manager

--- src/main/java/com/bas/chatapplication/ChatApplication.java
+++ src/main/java/com/bas/chatapplication/ChatApplication.java
@@ -1,7 +1,13 @@
 package com.bas.chatapplication;
 
+import com.bas.chatapplication.auth.AuthenticationService;
+import com.bas.chatapplication.auth.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.bas.chatapplication.user.Role.*;
 
 @SpringBootApplication
 public class ChatApplication {
@@ -9,5 +15,28 @@ public class ChatApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ChatApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner (AuthenticationService service) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("admin@email.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.firstname("Manager")
+					.lastname("Manager")
+					.email("manager@email.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+		};
 	}
 }
--- src/main/java/com/bas/chatapplication/auth/RegisterRequest.java
+++ src/main/java/com/bas/chatapplication/auth/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.bas.chatapplication.auth;
 
+import com.bas.chatapplication.user.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,4 +16,5 @@ public class RegisterRequest {
     private String lastname;
     private String email;
     private String password;
+    private Role role;
 }
