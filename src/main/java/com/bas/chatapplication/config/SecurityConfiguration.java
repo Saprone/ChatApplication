6a207500ed7142efbebccb2bb30f4f4ad982ba7d
Bas Onrust <74906654+Saprone@users.noreply.github.com>
2023-11-03 15:48:31 +0100

    Added SwaggerUI dependency and whitelisted

--- src/main/java/com/bas/chatapplication/config/SecurityConfiguration.java
+++ src/main/java/com/bas/chatapplication/config/SecurityConfiguration.java
@@ -22,6 +22,20 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
+    private static final String[] WHITE_LIST_URL =
+        {
+            "/api/v1/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html"
+        };
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
@@ -31,7 +45,7 @@ public class SecurityConfiguration {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(req ->
-                req.requestMatchers("/api/v1/auth/**")
+                req.requestMatchers(WHITE_LIST_URL)
                     .permitAll()
 
                     .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
