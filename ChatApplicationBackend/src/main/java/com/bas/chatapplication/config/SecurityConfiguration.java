82c042426b83c994f4988f02bf054736fdf5962b
Bas Onrust <74906654+Saprone@users.noreply.github.com>
2023-11-07 17:15:34 +0100

    Fixed CORS issue

--- ChatApplicationBackend/src/main/java/com/bas/chatapplication/config/SecurityConfiguration.java
+++ ChatApplicationBackend/src/main/java/com/bas/chatapplication/config/SecurityConfiguration.java
@@ -43,6 +43,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(AbstractHttpConfigurer::disable)
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(req ->
                 req.requestMatchers(WHITE_LIST_URL)
