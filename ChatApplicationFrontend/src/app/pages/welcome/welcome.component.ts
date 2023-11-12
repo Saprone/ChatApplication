9433b47fca0c125ba78045560014baee8bee582b
Bas Onrust <74906654+Saprone@users.noreply.github.com>
2023-11-13 00:59:21 +0100

    Created logout

--- ChatApplicationFrontend/src/app/pages/welcome/welcome.component.ts
+++ ChatApplicationFrontend/src/app/pages/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent {
+  constructor(private router: Router){
+  }
 
+  logout() {
+    localStorage.removeItem('token');
+
+    this.router.navigate(['login']);
+  }
 }
