475bd1ab4aa2b4fc05a39702e67833b40d68752c
Bas <onrust.bas@gmail.com>
2023-12-15 20:22:30 +0100

    Update TwoFactorAuthenticationServiceTests.java

--- ChatApplicationBackend/src/test/java/com/bas/chatapplication/mfa/TwoFactorAuthenticationServiceTests.java
+++ ChatApplicationBackend/src/test/java/com/bas/chatapplication/mfa/TwoFactorAuthenticationServiceTests.java
@@ -7,13 +7,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @RequiredArgsConstructor
 @SpringBootTest
-public class TwoFactorAuthenticationServiceTests {
+class TwoFactorAuthenticationServiceTests {
+
+    TwoFactorAuthenticationService twoFactorAuthenticationService = new TwoFactorAuthenticationService();
+    String secret = "c606de64c81a4c4e8549bac747ef5d15cef225efcbc79d15d3331f018614ea86";
+
+    @Test
+    void testGenerateQrCodeImageUri() {
+        Assertions.assertNotEquals("invalidCode", twoFactorAuthenticationService.generateQrCodeImageUri(secret));
+    }
 
     @Test
     void testIsOtpValid() {
-        String secret = "c606de64c81a4c4e8549bac747ef5d15cef225efcbc79d15d3331f018614ea86";
         String code = "123456";
-        TwoFactorAuthenticationService twoFactorAuthenticationService = new TwoFactorAuthenticationService();
 
         Assertions.assertNotEquals(true, twoFactorAuthenticationService.isOtpValid(secret, code));
     }
