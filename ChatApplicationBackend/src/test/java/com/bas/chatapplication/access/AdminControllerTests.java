464581951b1f27b6f76af4e30bf81aabd7168ecc
Bas <onrust.bas@gmail.com>
2023-12-17 14:26:02 +0100

    Added admincontroller unit tests

--- ChatApplicationBackend/src/test/java/com/bas/chatapplication/access/AdminControllerTests.java
+++ ChatApplicationBackend/src/test/java/com/bas/chatapplication/access/AdminControllerTests.java
@@ -2,15 +2,30 @@ package com.bas.chatapplication.access;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 public class AdminControllerTests {
 
-    AdminController adminController;
+    @MockBean
+    AdminController adminController = new AdminController();
 
     @Test
     public void testGet() {
-        adminController = new AdminController();
-
         Assertions.assertEquals("GET request successful from admin controller." ,(adminController.get()));
+    }
+
+    @Test
+    public void testPost() {
+        Assertions.assertEquals("POST request successful from admin controller." ,(adminController.post()));
+    }
+
+    @Test
+    public void testPut() {
+        Assertions.assertEquals("PUT request successful from admin controller." ,(adminController.put()));
+    }
+
+    @Test
+    public void testDelete() {
+        Assertions.assertEquals("DELETE request successful from admin controller." ,(adminController.delete()));
     }
 }
