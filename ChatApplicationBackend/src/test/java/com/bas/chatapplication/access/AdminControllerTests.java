51c0ca2d816aafe3f43d463f02c6cc76f747bf00
Bas <onrust.bas@gmail.com>
2023-12-17 14:01:01 +0100

    Update AdminControllerTests.java

--- ChatApplicationBackend/src/test/java/com/bas/chatapplication/access/AdminControllerTests.java
+++ ChatApplicationBackend/src/test/java/com/bas/chatapplication/access/AdminControllerTests.java
@@ -2,24 +2,15 @@ package com.bas.chatapplication.access;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
 public class AdminControllerTests {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private AdminController adminController;
+    AdminController adminController;
 
     @Test
-    public void testGet() throws Exception {
-        var getRequestAdminController = this.adminController.get();
+    public void testGet() {
+        adminController = new AdminController();
 
-        //Assertions.assertEquals("GET request successful from admin controller.", getRequestAdminController);
+        Assertions.assertEquals("GET request successful from admin controller." ,(adminController.get()));
     }
 }
