c20d192f2ffa0bffb8b961efe988fe2a8f214ea2
Bas <onrust.bas@gmail.com>
2023-12-18 17:38:06 +0100

    Update AuthenticationControllerTests.java

--- ChatApplicationBackend/src/test/java/com/bas/chatapplication/auth/AuthenticationControllerTests.java
+++ ChatApplicationBackend/src/test/java/com/bas/chatapplication/auth/AuthenticationControllerTests.java
@@ -1,18 +1,22 @@
-//package com.bas.chatapplication.auth;
+package com.bas.chatapplication.auth;
 
-/*import com.bas.chatapplication.auth.AuthenticationService;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import static com.bas.chatapplication.user.Role.ADMIN;
+import static com.bas.chatapplication.user.Role.MANAGER;
 
-
+@SpringBootTest
 public class AuthenticationControllerTests {
 
-    @MockBean
-    AuthenticationService authenticationService = new AuthenticationService();
+    @Autowired
+    AuthenticationService authenticationService;
 
     @Test
     void testRegister() {
@@ -27,32 +31,9 @@ public class AuthenticationControllerTests {
         var response = authenticationService.register(admin);
 
         Assertions.assertNotNull(response);
-
         Assertions.assertInstanceOf(String.class, response.getSecretImageUri());
         Assertions.assertInstanceOf(String.class, response.getAccessToken());
         Assertions.assertInstanceOf(String.class, response.getRefreshToken());
         Assertions.assertFalse(response.isMfaEnabled());
-
-        Assertions.assertEquals(HttpStatus.ACCEPTED, response);
     }
-
-    @Test
-    void testAuthenticate() {
-        ResponseEntity<Object> request = ;
-
-        var response = authetnicationService.authenticate(request);
-
-        Assertions.assertNotNull(response);
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
-    }
-
-    @Test
-    void testVerifyCode() {
-        ResponseEntity<Object> request = ;
-
-        var response = authetnicationService.verifyCode(request);
-
-        Assertions.assertNotNull(response);
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
-    }
-}*/
+}
