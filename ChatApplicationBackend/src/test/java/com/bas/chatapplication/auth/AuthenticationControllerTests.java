88717791a6b45b343941a732032a3c95332ec7d5
Bas Onrust <74906654+Saprone@users.noreply.github.com>
2023-12-18 22:48:29 +0100

    Update AuthenticationControllerTests.java

--- ChatApplicationBackend/src/test/java/com/bas/chatapplication/auth/AuthenticationControllerTests.java
+++ ChatApplicationBackend/src/test/java/com/bas/chatapplication/auth/AuthenticationControllerTests.java
@@ -5,12 +5,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import static com.bas.chatapplication.user.Role.ADMIN;
-import static com.bas.chatapplication.user.Role.MANAGER;
 
 @SpringBootTest
 class AuthenticationControllerTests {
