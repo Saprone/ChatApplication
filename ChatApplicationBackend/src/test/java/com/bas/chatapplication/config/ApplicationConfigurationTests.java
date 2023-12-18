74e053498d57c4d0f4e2e17435b24d42f5660393
Bas <onrust.bas@gmail.com>
2023-12-18 16:57:09 +0100

    Update ApplicationConfigurationTests.java

--- ChatApplicationBackend/src/test/java/com/bas/chatapplication/config/ApplicationConfigurationTests.java
+++ ChatApplicationBackend/src/test/java/com/bas/chatapplication/config/ApplicationConfigurationTests.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class ApplicationConfigurationTests {
+class ApplicationConfigurationTests {
 
     @Autowired
     private ApplicationConfiguration applicationConfiguration;
